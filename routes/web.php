028856f4de8be4ae1588d786ff87a216cf3fb8be
Petterson Abade <abadepetterson@gmail.com>
2025-04-13 15:36:34 -0300

    Add authentication and main controllers, login view, and Bootstrap assets

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    echo "Hello World";
-});
+// auth routes
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
