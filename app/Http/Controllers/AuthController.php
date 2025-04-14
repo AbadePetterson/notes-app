3bd590dac898cdab706eb2b8b086f3b6485a7045
Petterson Abade <abadepetterson@gmail.com>
2025-04-14 09:26:36 -0300

    Update login validation to require a valid email format for username and enforce password length constraints. Enhance error messages for better user feedback in the login form.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,9 +22,17 @@ class AuthController extends Controller
     public function login_submit(Request $request)
     {
        $request->validate([
-            'text_username' => 'required',
-            'text_password' => 'required'
-        ]); 
+            'text_username' => 'required|email',
+            'text_password' => 'required|min:6|max:16'
+        ],
+        [
+            'text_username.required' => 'O username é obrigatório',
+            'text_username.email' => 'O username deve ser um email válido',
+            'text_password.required' => 'A senha é obrigatória',
+            'text_password.min' => 'A senha deve ter pelo menos :min caracteres',
+            'text_password.max' => 'A senha deve ter menos :max caracteres'
+        ]
+    ); 
 
         $username = $request->input('text_username');
         $password = $request->input('text_password');
