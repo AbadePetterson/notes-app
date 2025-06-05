d0dd874e9228a0a62da3377260d3d89371992daa
nosrettep <abadepetterson@gmail.com>
2025-06-05 11:30:04 -0300

    Refactor login_submit method to streamline user authentication process and improve error handling

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -39,10 +39,27 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        //get all users from database
-        $users = User::all()->toArray();
+        //check if users exists
+        $user = User::where('username', $username)->where('deleted_at', null)->first();
 
-        echo '<pre>';
-        print_r($users);
+        if (!$user) {
+            return redirect()->back()->withInput()->with(['loginError' => 'user or password not found!']);
+        }
+
+        //check if password is correct
+        if(!password_verify($password, $user->password)){
+            return redirect()->back()->withInput()->with(['loginError' => 'user or password not found!']);
+        }
+
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        //login user
+        session([
+            'user' => $user->id,
+            'username' => $user->username
+        ]);
+
+        echo 'LOGIN SUCCESS';
     }
 }
